348ede49037eb6f8a604e5a1bd803811bc2f18b0
Aaron Cox <aaron.cox@greymass.com>
2013-01-25 16:10:36 -0600

    A test document, extended off Epic_Mongo_Document

---
+++ application/models/Test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author Aaron Cox
+ **/
+class Test extends Epic_Mongo_Document
+{
+	public $_collection = 'test';
+} // END class Laravel_Mongo_Document_Test extends Epic_Mongo_Document
