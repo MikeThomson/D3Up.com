95bcb5a15da84ab6b7ce654dcb90dd4cce87ee82
Aaron Cox <aaron.cox@greymass.com>
2013-01-25 16:10:57 -0600

    A configuration file for EpicMongo to connect and setup a default schema

---
+++ application/config/epicmongo.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'host' => '127.0.0.1',
+	'username' => null,
+	'password' => null,
+	'dbname' => 'laravel',
+	'models' => 'models',
+	'typemap' => array(
+		'test' => 'Test',
+	)
+);
