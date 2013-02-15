068decd966f43148b38b9a4e6546bd44649241a2
Aaron Cox <aaron.cox@greymass.com>
2013-02-15 15:54:44 -0600

    Changed Verbiage so it's Email or Username

--- application/views/user/login.blade.php
+++ application/views/user/login.blade.php
@@ -3,14 +3,14 @@
 {{ Form::open('login', 'test', array('class' => 'form-signin')) }}
 	<h2>Login</h2>
 	<!-- username field -->
-	<p>{{ Form::label('email', 'Email') }}</p>
+	<p>{{ Form::label('email', 'Email/Username') }}</p>
 	<p>{{ Form::text('email') }}</p>
 	<!-- password field -->
 	<p>{{ Form::label('password', 'Password') }}</p>
 	<p>{{ Form::password('password') }}</p>
 	<!-- submit button -->
 	@if (Session::has('login_errors'))
-		<div class="alert alert-error">Email or password incorrect.</div>
+		<div class="alert alert-error">Email/Username or Password incorrect.</div>
 	@endif
 	<p>
 		{{ Form::submit('Login', array('class' => 'btn')) }}
