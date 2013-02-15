6d03fd7c44884b08a83f81006ed302838e8e9e04
Aaron Cox <aaron.cox@greymass.com>
2013-02-15 15:59:40 -0600

    Added Message to the top of the page about it being developmental.

--- application/views/template/global/sysmsg.blade.php
+++ application/views/template/global/sysmsg.blade.php
@@ -1,3 +1,3 @@
-<!-- <div class="alert alert-error">
-	rawr
-</div> -->
+<div class="alert alert-error">
+	<strong>Notice</strong>: This is a developmental version of D3Up.com V2, MANY things don't work. 
+</div>
