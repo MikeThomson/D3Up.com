dea3193f90a003601f7df36f19a1c45d2290ceda
Aaron Cox <aaron.cox@greymass.com>
2013-02-15 15:19:35 -0600

    Build Views.

--- application/views/build/view.blade.php
+++ application/views/build/view.blade.php
@@ -1,5 +1,17 @@
-@layout('templates.main');
+@layout('template.main')
 
 @section('content')
-<?= $build->name ?>
+@include('build.section.header')
+<div class='row'>
+	<div class='span3'>
+		<ul class="nav nav-list">
+		  <li class="nav-header">List header</li>
+		  <li class="active"><a href="#">Home</a></li>
+		  <li><a href="#">Library</a></li>
+		  ...
+		</ul>
+	</div>
+	<div class='span6'>Body</div>
+	<div class='span3'>Stats</div>
+</div>
 @endsection
