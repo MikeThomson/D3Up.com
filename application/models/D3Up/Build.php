dfb72077b4d8cb69ae59f4992070eb383c98e658
Aaron Cox <aaron.cox@greymass.com>
2013-02-16 15:15:04 -0600

    Added some references to builds for different document types, however EpicMongo seems to not work with them atm. @gnarf37 we're gonna have to take a look at this ;)

--- application/models/D3Up/Build.php
+++ application/models/D3Up/Build.php
@@ -1,5 +1,11 @@
 <?php
 
 class D3Up_Build extends Epic_Mongo_Document {
+	
 	protected $_collection = 'builds';
+  protected $_typeMap = array(
+		'gear' => array('ref:D3Up_Item', 'req'),
+		'_createdBy' => array('doc:user', 'ref'),
+  );
+
 }
