900927ce627ae330f0b7427855a482aa38563ea7
Aaron Cox <aaron.cox@greymass.com>
2013-02-19 12:50:38 -0600

    Added a basic GitHub commit history for this repo to the homepage.

---
+++ application/tasks/Github.php
@@ -0,0 +1,22 @@
+<?php
+class Github_Task {
+	
+	protected $_rss = 'https://github.com/aaroncox/D3Up.com/commits/master.atom?login=aaroncox';
+	protected $_limit = 5;
+	
+	public function run($args) {
+		$token = Config::get('github.token');
+		$content = file_get_contents($this->_rss."&token=".$token);  
+    $rss = new SimpleXmlElement($content);
+		$data = array();
+		$idx = 1;
+		foreach($rss as $entry) {
+			if($entry->updated && $entry->title && $idx <= $this->_limit) {
+				$data[(string)$entry->updated] = (string)$entry->title;
+				$idx++;
+			}
+		}
+		Cache::put('github_commits', $data, 5);
+	}
+	
+}
--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -14,6 +14,13 @@
 		<div class='span3'>
 			<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/D3Up" data-widget-id="302248766797398016">Tweets by @D3Up</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			@if(Cache::has('github_commits'))
+			<table class='recent-commits table'>
+				@foreach(Cache::get('github_commits') as $commit)
+					<tr><td>{{ $commit }}</td></tr>
+				@endforeach
+			</table>
+			@endif
 		</div>
 	</div>
 </div>
