bbd2b60f4d3546f58d496d0e56947d490a4b3aa0
Aaron Cox <aaron.cox@greymass.com>
2013-02-20 15:52:09 -0600

    Removing icon for now (until I can fix, it's being escaped).

--- application/binders/itemIcon.binder.php
+++ application/binders/itemIcon.binder.php
@@ -3,5 +3,5 @@ return array('html itemIcon' => function($item) {
 	if(!$item->icon) {
 		return null;
 	}
-	return HTML::image('http://media.blizzard.com/d3/icons/items/small/'.$item->icon.'.png', $item->name);
+	// return HTML::image('http://media.blizzard.com/d3/icons/items/small/'.$item->icon.'.png', $item->name);
 });
