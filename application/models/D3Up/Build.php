69746d6ff0beb1674144530f6a90d130d138749a
Aaron Cox <aaron.cox@greymass.com>
2013-02-20 16:50:09 -0600

    Changed ->gear from a DocumentSet to a Document (D3Up_Gear)

--- application/models/D3Up/Build.php
+++ application/models/D3Up/Build.php
@@ -4,11 +4,7 @@ class D3Up_Build extends Epic_Mongo_Document {
 	
 	protected $_collection = 'builds';
   protected $_typeMap = array(
-		'gear' => array('set:item', 'auto'),
-		// 'set:gear' => 'Test_DocumentSet_DocumentSet',
-		// 	
-		// 'gear' => array('set', 'auto'),
-		// 'gear.$' => array('ref:D3Up_Items', 'req'),	
+		'gear' => array('doc:gearset', 'auto'),
 		'_createdBy' => array('doc:user', 'ref'),
   );
 
