634ba7561ea2b26e09422a082ccc4d7d462b21df
Aaron Cox <aaron.cox@greymass.com>
2013-02-20 16:50:49 -0600

    Added some sample stuff to skills.

--- application/views/build/section/skills.blade.php
+++ application/views/build/section/skills.blade.php
@@ -1,2 +1,29 @@
-<h3>Skills / Passives</h3>
-<p>Breakdown of every skill, it's individual DPS and benefits, one at a time.</p>
+<div>
+  <ul class="breadcrumb">
+    <li><a href="#skills-overview" data-toggle="tab">Overview</a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="gear-overview">
+			Overview
+			<p><img src="http://placehold.it/500x100&text=Skill #1"></p>
+			<p><img src="http://placehold.it/500x100&text=Skill #2"></p>
+			<p><img src="http://placehold.it/500x100&text=Skill #3"></p>
+			<p><img src="http://placehold.it/500x100&text=Skill #4"></p>
+			<p><img src="http://placehold.it/500x100&text=Skill #5"></p>
+			<p><img src="http://placehold.it/500x100&text=Skill #6"></p>
+    </div>
+    <div class="tab-pane" id="gear-contributions">
+			Contributions
+    </div>
+  </div>
+</div>
+<script type="text/javascript" charset="utf-8">
+	$('#gear-overview a[data-toggle="tab"]').on('show', function (e) {
+		$('#gear-overview .nav').hide();
+		$('#gear-overview .return-home').bind('click', function() {
+			$(this).closest('.item-detail').removeClass("active");
+			$('#gear-overview .nav').show();
+		});
+		// console.log(e.target);
+	})
+</script>
