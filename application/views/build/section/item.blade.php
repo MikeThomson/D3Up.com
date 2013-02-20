3dc5ddd70fefb406942d1836585814e028562fab
Aaron Cox <aaron.cox@greymass.com>
2013-02-20 16:19:50 -0600

    Removed # from link on Breadcrumb.

--- application/views/build/section/item.blade.php
+++ application/views/build/section/item.blade.php
@@ -1,6 +1,6 @@
 <div class="tab-pane item-detail" id="tab-item-{{$item->id}}">
 	<ul class="breadcrumb">
-	  <li><a class='return-home' href="#">Items</a> <span class="divider">/</span></li>
+	  <li><a class='return-home'>Items</a> <span class="divider">/</span></li>
 	  <li>{{ HTML::itemLink($item) }}</li>
 	</ul>
 	<img src="http://placehold.it/500x400&text=Item Tooltip">
