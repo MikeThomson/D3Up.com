93c489f02da12bdcf61d897dd33c341b02ac8480
Aaron Cox <aaron.cox@greymass.com>
2013-03-04 17:01:42 -0600

    Removed game data and calc from local, using D3Up's live version (for compatibility). Also added handlebarsjs for templating and experimentation.

--- application/views/template/global/scripts.blade.php
+++ application/views/template/global/scripts.blade.php
@@ -3,14 +3,15 @@
 <script src="/js/jquery-ui.js"></script>
 <script src="/js/underscore.js"></script>
 <script src="/js/bootstrap.js"></script>
+<script src="/js/handlebars.js"></script>
 <!-- Battle.net Scripts -->
 <script src="http://us.battle.net/d3/static/js/tooltips.js"></script>
 <!-- D3Up Scripts -->
 <script src="/js/d3up.js"></script>
-<script src="/js/game/data.js"></script>
+<script src="http://d3up.com/js/gamedata.js"></script>
 <script src="/js/build.js"></script>
-<script src="/js/calc.js"></script>
-<script src="/js/utils/itembuilder.js"></script>
+<script src="http://d3up.com/js/unmin/calcv2.js"></script>
+<script src="http://d3up.com/js/unmin/itembuilder.js"></script>
 <script src="/js/utils/tooltip.js"></script>
 <!-- The D3Up Tooltip Div -->
 <div id="d3up-tooltip"></div>
