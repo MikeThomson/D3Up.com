8d42baaca46896d203057154388ec165bb439517
Aaron Cox <aaron.cox@greymass.com>
2013-03-15 10:44:17 -0500

    Added small amount of verbiage on the math page, explaining what it is.

--- application/views/math/index.blade.php
+++ application/views/math/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <div class='row'>
 	<div class='span9'>
+		<h1>Diablo 3 Math - Explanations</h1>
+		<p>Listed below you will find many common terms and formula's used in Diablo 3. Click on any of them to view an explanation of what they are, how they're generated and help you better understand your characters.</p>
 		<ul>
 		@foreach($math as $entry)
 			<li><a href='/math/{{ $entry->id }}'>{{ $entry->title }}</a></li>
