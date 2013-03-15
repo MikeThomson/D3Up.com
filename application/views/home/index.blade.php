205804bccc11bd5b4f7ba4c36780af412564ed64
Aaron Cox <aaron.cox@greymass.com>
2013-03-15 10:48:17 -0500

    Added some text about math to the homepage.

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -15,6 +15,7 @@
 					<li><a href="/b/1">Viewing Builds</a> (http://v2.d3up.com/b/###### insert your number)</li>
 					<li><a href="http://cl.ly/image/1G2Y0W313Z2r">Preview of the new Sync Screen</a></li>
 					<li>Localization Enabled - You can change from 'English' to 'Pig Latin' using the dropdown in the upper right of the menubar on the top (thanks to allan on #diablo for the idea!)</li>
+					<li>'Math' section added - A community 'wiki' type engine that will allow players to learn about different game mechanics.</li>
 				</ul>
 				@if(Cache::has('github_commits'))
 				<table class='recent-commits table'>
