f918c914b54357db9906e86d64586b2bc053c980
Aaron Cox <aaron.cox@greymass.com>
2013-03-15 11:18:35 -0500

    Added link to Math for creating a new entry.

--- application/views/math/index.blade.php
+++ application/views/math/index.blade.php
@@ -5,6 +5,7 @@
 	<div class='span9'>
 		<h1>Diablo 3 Math - Explanations</h1>
 		<p>Listed below you will find many common terms and formula's used in Diablo 3. Click on any of them to view an explanation of what they are, how they're generated and help you better understand your characters.</p>
+		<p>Have one you think needs adding? <a href='/math/create' class='btn'>Create a new one!</a></p>
 		<ul>
 		@foreach($math as $entry)
 			<li><a href='/math/{{ $entry->id }}'>{{ $entry->title }}</a></li>
