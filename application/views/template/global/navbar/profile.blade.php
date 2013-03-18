9de32651368a45335075b08c111412793d49cb25
Aaron Cox <aaron.cox@greymass.com>
2013-03-18 11:47:01 -0500

    Changed the Logout button to use the Locale file.

--- application/views/template/global/navbar/profile.blade.php
+++ application/views/template/global/navbar/profile.blade.php
@@ -4,6 +4,6 @@
 		<b class="caret"></b>
 	</a>
 	<div class="dropdown-menu">
-		<a href="/logout" class='btn btn-mini btn-block'>{{ __('generic.logout') }}</a>
+		<a href="/logout" class='btn btn-mini btn-block'>{{ __('login.logout') }}</a>
 	</div>
 </li>
