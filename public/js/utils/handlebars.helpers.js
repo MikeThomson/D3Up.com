2fc176cc59797f5f6f5d01086c52b6dd5f343435
Aaron Cox <aaron.cox@greymass.com>
2013-03-20 21:42:32 -0500

    Added a handlebars helper called prettyStat, that you pass a stat and value. It determines how it should be rendered.

--- public/js/utils/handlebars.helpers.js
+++ public/js/utils/handlebars.helpers.js
@@ -1,4 +1,36 @@
-Handlebars.registerHelper('round', function(number,decimal_points) {
+Handlebars.registerHelper('prettyStat', function(value, stat) {
+	
+	var special = {
+				decimals: {
+					'aps-mh': 4,
+					'aps-oh': 4,				
+				}, 
+				percents: [
+					'percent-melee-reduce',
+					'percent-elite-reduce',
+					'percent-range-reduce',
+				]
+			},
+			decimals = 2,
+			prepend = "",
+			append = "";
+	if(stat && special.decimals[stat]) {
+		decimals = special.decimals[stat];
+	}
+	if(stat && _.indexOf(special.percents, stat) >= 0) {
+		value *= 100;
+		append = "%";
+	}
+	if(typeof(value) == "undefined" || isNaN(value)) {
+		value = 0;
+	}
+	if(value) {
+		console.log(stat, value, decimals);
+		value = Handlebars.helpers.round.call(this, value, decimals);		
+	}
+	return prepend + value + append;
+});
+Handlebars.registerHelper('round', function(number, decimal_points) {
 	if(typeof(number) == "undefined") {
 		return 0;
 	}
