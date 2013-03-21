33610b0a80f0f48b9e9604ca586cb0d2c78b7979
Aaron Cox <aaron.cox@greymass.com>
2013-03-20 22:11:03 -0500

    Fix for the Green Arrow/Compare CSS for non-chrome browsers.

--- application/views/build/section/stats/table.blade.php
+++ application/views/build/section/stats/table.blade.php
@@ -18,11 +18,13 @@
 					<td>{{ __('diablo.'.$stat) }}</td>
 					@endif
 					<td>
-						{{ HTML::hb('#if_gt victor.'.$stat.' compare=0') }}<span class='victor-{{ HTML::hb('victor.'.$stat) }}'></span>{{ HTML::hb('/if_gt') }}
-						@if($id == 'compare')
-							{{ HTML::hb('#if_gt stats.'.$stat.' compare=0') }}+{{ HTML::hb('/if_gt') }}
-						@endif
-						{{ HTML::hb('prettyStat stats.'.$stat.' "'.$stat.'"') }}
+						<div class="stat-container">
+							{{ HTML::hb('#if_gt victor.'.$stat.' compare=0') }}<span class='victor-{{ HTML::hb('victor.'.$stat) }}'></span>{{ HTML::hb('/if_gt') }}
+							@if($id == 'compare')
+								{{ HTML::hb('#if_gt stats.'.$stat.' compare=0') }}+{{ HTML::hb('/if_gt') }}
+							@endif
+							{{ HTML::hb('prettyStat stats.'.$stat.' "'.$stat.'"') }}
+						</div>
 					</td>
 				</tr>
 				@endforeach
