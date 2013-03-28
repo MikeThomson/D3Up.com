17fe7e66525901d694a67378c20441914cc3764f
Aaron Cox <aaron.cox@greymass.com>
2013-03-27 21:00:38 -0500

    Added a function to right align text. Also changed the signature so if you have a paragon level, it shows that instead of level. You're obviously 60.

--- application/libraries/D3Up/Signature.php
+++ application/libraries/D3Up/Signature.php
@@ -12,11 +12,11 @@ class D3Up_Signature {
 		$this->addText($build->name, 30, 7, 32, "#000");	// Simulated Shadow
 		$this->addText($build->name, 30, 5, 30, "#70B1D5");	// Insert Build Name
 		// Add the Level Information
-		$levelText = "Level ".$build->level;
+		$levelText = "Level: ".$build->level;
 		if($build->paragon) {
-			$levelText .= " (".$build->paragon.")";
+			$levelText = "Paragon: ".$build->paragon;
 		}
-		$this->addShadowText($levelText, 12, 15, 60, "#fff", 'sans');	// Insert Build Name
+		$this->addShadowText($levelText, 12, 15, 55, "#fff", 'sans');	// Insert Build Name
 		imagefilledrectangle($this->_image, 0, 65, 450, 450, imagecolorallocatealpha($this->_image, 0, 0, 0, 50));
 		// Add Skill Icons
 		if($build->actives) {
@@ -41,8 +41,7 @@ class D3Up_Signature {
 		if(isset($build->stats['ehp'])) {
 			$this->addShadowText("EHP: ".HTML::prettyStat($build->stats['ehp'],2), 10, 370, 94, "#fff", 'sans');	// Insert Build Name			
 		}
-		$this->addShadowText("d3up.com", 10, 385, 14, "#fff", 'sans');
-
+		$this->addShadowText("d3up.com", 10, 385, 14, "#fff", 'sans', 'right');
 		// Sample Generate for Debugging
 		header("Content-type: image/png");
 		imagepng($this->_image);
@@ -50,15 +49,22 @@ class D3Up_Signature {
 		exit;
 	}
 	
-	public function addShadowText($text, $size, $x, $y, $hex = "#fff", $font = "d3") {
-		$this->addText($text, $size, $x-1, $y-1, "#000", $font);
-		$this->addText($text, $size, $x+1, $y+1, "#000", $font);
-		$this->addText($text, $size, $x+2, $y+2, "#000", $font);
-		$this->addText($text, $size, $x, $y, $hex, $font);
+	public function addShadowText($text, $size, $x, $y, $hex = "#fff", $font = "d3", $align = null) {
+		$this->addText($text, $size, $x-1, $y-1, "#000", $font, $align);
+		$this->addText($text, $size, $x+1, $y+1, "#000", $font, $align);
+		$this->addText($text, $size, $x+2, $y+2, "#000", $font, $align);
+		$this->addText($text, $size, $x, $y, $hex, $font, $align);
 	}
 	 
-	public function addText($text, $size, $x, $y, $hex = "#fff", $font = "d3") {
+	public function addText($text, $size, $x, $y, $hex = "#fff", $font = "d3", $align = null) {
 		$color = $this->hex2rgb($hex);
+		if($align == 'right') {
+			$dimensions = imagettfbbox($size, 0, path('app') . "signatures/fonts/".$font.".ttf", $text);
+			$textWidth = abs($dimensions[4] - $dimensions[0]);
+			$x = imagesx($this->_image) - $textWidth - 10;			
+			// var_dump($x, $textWidth, imagesx($this->_image)); exit;
+		}
+		
 		imagettftext(
 			$this->_image, 
 			$size, 
