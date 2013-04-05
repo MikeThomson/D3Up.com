37543610d579b5b309f0d10b88c7127abab36197
Aaron Cox <aaron.cox@greymass.com>
2013-04-05 14:01:48 -0500

    Changed URL to sigs.d3up.com

--- application/views/build/signature.blade.php
+++ application/views/build/signature.blade.php
@@ -10,7 +10,7 @@
 <div class='content-page'>
 <p>We've generated a forum signature for you to use! This is placeholder text, and I should put some info here.</p>
 <p><img src='http://sigs.d3up.com.s3.amazonaws.com/b/{{ $build->id }}.jpg'></p>
-<p><input type='text' value='http://sigs.d3up.com.s3.amazonaws.com/b/{{ $build->id }}.jpg' style='width: 600px'></p>
+<p><input type='text' value='http://sigs.d3up.com/b/{{ $build->id }}.jpg' style='width: 600px'></p>
 @if($build->_createdBy && Auth::user() && Auth::user()->id == $build->_createdBy->id)
 <p>If you need to refresh your signature, click the button below.</p>
 <form method='post'>
