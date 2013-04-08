d53c2b5f27255e20e3ab21e84e31e02f1b109dbb
Aaron Cox <aaron.cox@greymass.com>
2013-04-08 16:44:50 -0500

    Simplified bootstrap

--- application/tests/bootstrap.php
+++ application/tests/bootstrap.php
@@ -11,32 +11,11 @@ define('DS', DIRECTORY_SEPARATOR);
 require 'paths.php';
 
 // --------------------------------------------------------------
-// Override the application paths when testing the core.
-// --------------------------------------------------------------
-// $path = path('sys').'tests'.DS;
-
-// set_path('app', $path.'application'.DS);
-// 
-// set_path('bundle', $path.'bundles'.DS);
-// 
-// set_path('storage', $path.'storage'.DS);
-
-// --------------------------------------------------------------
 // Bootstrap the Laravel core.
 // --------------------------------------------------------------
 require path('sys').'core.php';
-
-Laravel\Event::listen(Laravel\Config::loader, function($bundle, $file)
-{
-	return Laravel\Config::file($bundle, $file);
-});
 
 // --------------------------------------------------------------
 // Start the default bundle.
 // --------------------------------------------------------------
 Laravel\Bundle::start(DEFAULT_BUNDLE);
-
-// Laravel\Bundle::register('epic_mongo', array('auto' => true));
-
-include(path('app').'../bundles/epic_mongo/Mongo.php');
-// Laravel\Bundle::start('myunit');
