96e3406222a7d8d501fd7e3522414ccd4ca260a9
Aaron Cox <aaron.cox@greymass.com>
2013-04-08 16:11:03 -0500

    Need the test bootstrap.

---
+++ application/tests/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+// --------------------------------------------------------------
+// Define the directory separator for the environment.
+// --------------------------------------------------------------
+define('DS', DIRECTORY_SEPARATOR);
+
+// --------------------------------------------------------------
+// Set the core Laravel path constants.
+// --------------------------------------------------------------
+require 'paths.php';
+
+// --------------------------------------------------------------
+// Override the application paths when testing the core.
+// --------------------------------------------------------------
+$path = path('sys').'tests'.DS;
+
+// set_path('app', $path.'application'.DS);
+// 
+// set_path('bundle', $path.'bundles'.DS);
+// 
+// set_path('storage', $path.'storage'.DS);
+
+// --------------------------------------------------------------
+// Bootstrap the Laravel core.
+// --------------------------------------------------------------
+require path('sys').'core.php';
+
+// --------------------------------------------------------------
+// Start the default bundle.
+// --------------------------------------------------------------
+Laravel\Bundle::start(DEFAULT_BUNDLE);
+
+Laravel\Bundle::register('epic_mongo', array('auto' => true));
+
+include(path('app').'../bundles/epic_mongo/Mongo.php');
+// Laravel\Bundle::start('myunit');
