4e2c7ba4ff6db7dcf18c946ebb968bade8621e68
Aaron Cox <aaron.cox@greymass.com>
2013-04-13 13:18:39 -0500

    Included missing template file for the Math section.

---
+++ application/views/math/breadcrumb.blade.php
@@ -0,0 +1,16 @@
+<ul class='breadcrumb'>
+	<li><a href='/math'>Math</a></li>
+	@if(isset($math) && isset($lang))
+		<li><span class="divider">/</span></li>
+		<li><a href='/math/{{ $math->id }}'>{{ $math->_localized[$lang]['title'] }}</a></li>
+	@endif
+	@if(Auth::user())
+	<li class='btn-group pull-right'>
+		@if(isset($math) && isset($lang))
+		<a href='/math/1/edit?language={{$lang}}' class='btn btn-mini'>Edit</a>
+		<a href='/math/1/edit' class='btn btn-mini'>Translate</a>
+		@endif
+		<a href='/math/create' class='btn btn-mini'>Create New</a>
+	</li>
+	@endif
+</ul>
