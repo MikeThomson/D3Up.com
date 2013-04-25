6fb24ba71cfa7551588e7134a9e000537f07cf8c
Aaron Cox <aaron.cox@greymass.com>
2013-04-25 18:10:51 -0500

    Fixed another index

--- notes/api-indexes.js
+++ notes/api-indexes.js
@@ -2,6 +2,6 @@
 db.builds.ensureIndex({public: 1});
 db.builds.ensureIndex({public: 1, 'stats.ehp': -1}, {sparse: true});
 db.builds.ensureIndex({public: 1, 'stats.dps': -1}, {sparse: true});
-db.builds.ensureIndex({public: 1, class: 1, 'skills': 1}, {sparse: true});
+db.builds.ensureIndex({actives: 1, class: 1, public: 1}, {sparse: true});
 db.builds.ensureIndex({public: 1, class: 1, 'stats.dps': -1}, {sparse: true});
 db.builds.ensureIndex({public: 1, class: 1, 'stats.ehp': -1}, {sparse: true});
