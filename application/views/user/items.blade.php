a15b8fe1a7163a492f2b5aa5a668d66028fb9dc8
Aaron Cox <aaron.cox@greymass.com>
2013-04-27 12:12:19 -0500

    Added placeholder for user/items

---
+++ application/views/user/items.blade.php
@@ -0,0 +1,27 @@
+@layout('template.main')
+
+@section('styles')
+<link href="/css/build.css" rel="stylesheet">
+<link href="/css/compare.css" rel="stylesheet">
+<link href="/css/paperdoll.css" rel="stylesheet">
+<link href="/css/utils/chosen.css" rel="stylesheet">
+@endsection
+
+@section('scripts')
+<script src="http://d3up.com/js/gamedata.js"></script>
+<script src="/js/build.js"></script>
+<script src="/js/utils/compare.js"></script>
+<script src="http://d3up.com/js/unmin/calcv2.js"></script>
+<script src="http://d3up.com/js/unmin/itembuilder.js"></script>
+<script src="/js/utils/chosen.min.js"></script>
+@endsection
+
+@section('content')
+	<h1>My Items</h1>
+	
+	<script type="text/javascript" charset="utf-8">
+		$(function() {
+			
+		});
+	</script>
+@endsection
