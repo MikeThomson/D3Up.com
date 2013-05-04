072752c37e8662ab02e5fe24a3b821c1d695ed0c
Aaron Cox <aaron.cox@greymass.com>
2013-05-03 20:21:04 -0500

    Fixed User Class to search for only users.. still need to implement in EpicMongo.

--- application/models/D3Up/User.php
+++ application/models/D3Up/User.php
@@ -8,5 +8,18 @@
 class D3Up_User extends Epic_Mongo_Document
 {
 	protected $_collection = 'users';
+	protected static $_documentType = 'user';
+	
+	// This should be embedded into something we extend?
+	public function findOne($query = array(), $fields = array()) {
+		$query['_type'] = static::$_documentType;
+		return parent::findOne($query, $fields);
+	}
+
+	// This should be embedded into something we extend?
+	public function find($query = array(), $fields = array()) {
+		$query['_type'] = static::$_documentType;
+		return parent::find($query, $fields);
+	}
 	
 } // END class D3Up_User extends Epic_Mongo_Document
