e96ea3ddcde0eedac1b637381c7ca8400d9a6c5c
Aaron Cox <aaron.cox@greymass.com>
2013-05-06 11:08:40 -0500

    Changed the Dashboard to have a bit more info.

--- application/views/user/index.blade.php
+++ application/views/user/index.blade.php
@@ -8,14 +8,22 @@ User Dashboard for {{ $user->username }}
 	<div class='row-fluid'>
 		<div class='span9 content-page'>
 			<div class='btn-group pull-right'>
+				<a href="/build/create" class='btn btn-success'>Create a Build</a>
 				<a href="/user/edit" class='btn'>Edit Profile</a>
 				<a href="/password" class='btn'>Change Password</a>
 			</div>
 			@include('user.checks')
-			<h2>{{ $user->username }} - {{ $user->email ?: 'No Email Specified' }}</h2>
+			<h2>Welcome {{ $user->username }}!</h2>
+			<p>This page will be used for quick access to all of your information. It's empty right now however except some user information below.</p>
+			<ul>
+				<li>Username: "{{ $user->username }}"</li>
+				<li>Email: "{{ $user->email }}"</li>
+				<li>BattleTag: "{{ $user->battletag }}"</li>
+				<li>Region: "{{ $user->region }}"</li>
+			</ul>
 		</div>
 		<div class='span3 content-page'>
 			
 		</div>
 	</div>
-@endsection
+@endsection	
