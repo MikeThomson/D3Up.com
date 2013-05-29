3a2ad80123da0ded45ce62e9c2c12380d2aefc04
Aaron Cox <aaron.cox@greymass.com>
2013-05-28 19:03:52 -0500

    Bunch more features mentioned.

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -13,10 +13,13 @@ D3Up.com - Diablo 3 Build Calculator for EHP and DPS
 			<p>Below is the update history from Github chronicling the development of V2. Some of it probably won't make much sense to non-techies, but you'll see new features being added as they come along. I'll try to feature some links to things as well as they become available.</p>
 			<h3>Currently Available</h3>
 			<ul>
-				<li><a href="/b/1">Viewing Builds</a> (http://v2.d3up.com/b/###### insert your ID)</li>
+				<li><a href="/build">New jQuery Build Browser</a> for browsing all builds.</li>
+				<li><a href="http://github.com/d3up/api">New API</a> for serving out build information.</li>
+				<li><a href="/b/1">Viewing a Build</a> (http://v2.d3up.com/b/###### insert your ID)</li>
 				<li><a href="/c/1/2">Build Comparisons</a> (http://v2.d3up.com/c/##/## insert two IDs of builds)</li>
 				<li><a href="http://github.com/d3up/Calculator">Started construction</a> of new Calculator (pure JS implementation)</li>
 				<li><a href="http://cl.ly/image/1G2Y0W313Z2r">Preview of the new Sync Screen</a></li>
+				<li>Helper "Tooltips" on common page to help explain how things work.</li>
 				<li>Localization Enabled - You can change from 'English' to 'Pig Latin' using the dropdown in the upper right of the menubar on the top (thanks to allan on #diablo for the idea!)</li>
 				<li>'<a href="/math">Math</a>' section added - A community 'wiki' type engine that will allow players to learn about different game mechanics.</li>
 				<li>'<a href="/guide">Guide</a>' section added - Formatting still broken, editing not working yet.</li>
