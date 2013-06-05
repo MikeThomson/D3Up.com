24cb0ae2e8252f773f907e1c8ab691102e22e931
Aaron Cox <aaron.cox@greymass.com>
2013-06-04 19:11:25 -0500

    Revamped compare page to use new calculator mechanics.

--- application/views/build/compare.blade.php
+++ application/views/build/compare.blade.php
@@ -4,11 +4,10 @@
 @endsection
 
 @section('scripts')
-<script src="http://d3up.com/js/gamedata.js"></script>
 <script src="/js/build.js"></script>
 <script src="/js/utils/compare.js"></script>
-<script src="http://d3up.com/js/unmin/calcv2.js"></script>
-<script src="http://d3up.com/js/unmin/itembuilder.js"></script>
+<script src="http://phalcon.d3up.com/builds/{{ $build1->id }}.js"></script>
+<script src="http://phalcon.d3up.com/builds/{{ $build2->id }}.js"></script>
 @endsection
 
 @section('content')
@@ -77,106 +76,55 @@
 		</div>
 	</div>
 </div>
-<div id='character1' data-json='{{ json_encode($build1->json()) }}'></div>
-<div id='character2' data-json='{{ json_encode($build2->json()) }}'></div>
 <script>
-	// Build #1
-	var data1 = $("#character1").data("json"),
-			skills1 = {
-	        actives: data1.actives,
-	        passives: data1.passives
-	      },
-      gear1 = $("#build1-gear .gear-table a[data-json]"),
-      meta1 = {
-        level: data1.level,
-        paragon: data1.paragon,
-        heroClass: data1.heroClass,
-      },
-      build1 = new d3up.Build({
-        gear: gear1, 
-        skills: skills1,
-        meta: meta1
-      });
-	// Build #2
-	var data2 = $("#character2").data("json"),
-			skills2 = {
-	        actives: data2.actives,
-	        passives: data2.passives
-	      },
-      gear2 = $("#build2-gear .gear-table a[data-json]"),
-      meta2 = {
-        level: data2.level,
-        paragon: data2.paragon,
-        heroClass: data2.heroClass,
-      },
-      build2 = new d3up.Build({
-        gear: gear2, 
-        skills: skills2,
-        meta: meta2
-      });	
-	// Store the Builds
-	d3up.builds = {
-		build1: build1,
-		build2: build2
-	};
-	// Run stats
-	d3up.builds.build1.run();
-	d3up.builds.build2.run();
-	
+
+	var build1 = d3up.getBuild({{ $build1->id }});
+	var build2 = d3up.getBuild({{ $build2->id }});
+
 	var source   = $("#stats-sidebar-1").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build1;
-	$("#stats-sidebar-1").replaceWith(template(data));		
-
+	$("#stats-sidebar-1").replaceWith(template(build1));		
 	
 	var source   = $("#build-header-1 .skill-row").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build1;
-	$("#build-header-1 .skill-row").replaceWith(template(data));				
+	$("#build-header-1 .skill-row").replaceWith(template(build1));				
 
 	var source   = $("#build-header-2 .skill-row").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build2;
-	$("#build-header-2 .skill-row").replaceWith(template(data));				
+	$("#build-header-2 .skill-row").replaceWith(template(build2));				
 
 
 	var source   = $("#stats-sidebar-2").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build2;
-	$("#stats-sidebar-2").replaceWith(template(data));		
+	$("#stats-sidebar-2").replaceWith(template(build2));		
 	
 	var compare = new d3up.Compare;
 	var diff = compare.diff(build1, build2);
 
 	var source   = $("#stats-sidebar-compare").html();
 	var template = Handlebars.compile(source);
-	var data = diff;
-	$("#stats-sidebar-compare").replaceWith(template(data));
+	$("#stats-sidebar-compare").replaceWith(template(diff));
 
 	var source   = $("#mitigation-sidebar-compare").html();
 	var template = Handlebars.compile(source);
-	var data = diff;
-	$("#mitigation-sidebar-compare").replaceWith(template(data));
+	$("#mitigation-sidebar-compare").replaceWith(template(diff));
 
 	var source   = $("#build1-skills #skill-overview").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build1;
-	$("#build1-skills .tab-content").replaceWith(template(data));		
+	$("#build1-skills .tab-content").replaceWith(template(build1));		
 	
 	var source   = $("#build2-skills #skill-overview").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build2;
-	$("#build2-skills .tab-content").replaceWith(template(data));		
+	$("#build2-skills .tab-content").replaceWith(template(build2));		
 
 	var source   = $("#mitigation-sidebar-1").html();
 	var template = Handlebars.compile(source);
-	var data = d3up.builds.build1;
-	$("#mitigation-sidebar-1").replaceWith(template(data));		
+	$("#mitigation-sidebar-1").replaceWith(template(build1));		
 
 	var source   = $("#mitigation-sidebar-2").html();
 	var template = Handlebars.compile(source);
 	var data = d3up.builds.build2;
-	$("#mitigation-sidebar-2").replaceWith(template(data));		
+	$("#mitigation-sidebar-2").replaceWith(template(build2));		
 
 	
 	<?= (isset($_GET['debug'])) ? 'console.log(d3up.builds);' : ''; ?>
