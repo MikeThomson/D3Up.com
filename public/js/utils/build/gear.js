b5de5bf64b04794a529cc7f13c02fa7207a8b9a8
Aaron Cox <aaron.cox@greymass.com>
2013-06-24 19:09:32 -0500

    Gear simulation sorta works

--- public/js/utils/build/gear.js
+++ public/js/utils/build/gear.js
@@ -17,6 +17,10 @@
 		_create: function() {
 			// Load up our Calc
 			this.calc = this.options.calc;
+			// Storage for a modified build
+			this.modified = _.cloneDeep(this.options.calc.build);
+			// Create our simulation sandbox
+			this.sandbox = new d3up.Sandbox();
 			// Create an empty elements object
 			this.elements = {};
 			// Create an empty modifying ojbect
@@ -120,13 +124,17 @@
 						display = displays.find(search).clone();
 				// If we haven't initalized an item builder for this, do it
 				if(data === false) {
+					console.log(calc.build);
 					// Create the itemBuilder on the element
 					display.itemBuilder({
-						item: calc.build.gear[slot],
+						// Is the build authentic, do we need to warn the user?
+						authentic: calc.build.authentic,
+						// The item we're editing
+						item: _.cloneDeep(calc.build.gear[slot]),
+						// Modding starts the editor immediately
 						modding: true,
-						// onSave: function() {
-						// 	$this._modifyEnd(slot)
-						// }
+						// Our callbacks
+						onUpdate: $.proxy($this, '_modifyItem', slot),
 						onCancel: function() {
 							$this._modifyEnd(slot);
 						}
@@ -138,6 +146,44 @@
 				}
 			}, this);
 			
+		},
+		_modifyItem: function(slot, modifications) {
+			console.log("_modifyItem", slot, modifications);
+			// Create a fresh copy of the original item
+			var original = _.cloneDeep(this.calc.build.gear[slot]);
+			// Do we have modifications?
+			if($.isEmptyObject(modifications)) {
+				// Set the item to the original
+				this.modified.gear[slot] = original;
+				console.log("reverting", original);
+			} else {
+				// Modify the item and apply the modifications
+				this.modified.gear[slot] = _.merge(original, modifications);				
+			}
+			// Run the simulation
+			this.simulate();
+		},
+		simulate: function() {
+			console.log("_simulate", this.modified);
+			this.sandbox.reset();
+			// Add the "Compare" Build
+			this.sandbox.add(new d3up.Calc(this.modified, true), 'compare');			
+			// Add the "Original" Build
+			this.sandbox.add(this.calc, 'original');
+			// Render the simulation results
+			this._simulateRender();
+		},
+		_simulateRender: function() {
+			console.log('_simulateRender');
+			var target = this.options.resultsPane,
+					template = Handlebars.compile(this.options.resultsTemplate.html()),
+					content = template({
+						original: this.sandbox.get('original'),
+						compare: this.sandbox.get('compare'),
+						diff: this.sandbox.compare('compare_original')
+					});
+			// Replace the container with our handlebars content
+			target.find(".results").html($(content));
 		},
 		update: function () {
 			console.log("update");
